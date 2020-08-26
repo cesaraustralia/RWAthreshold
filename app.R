394f3937508a587d0946d545e40a6733707d3236
jamesmaino <jamesmaino@gmail.com>
2020-08-26 12:20:55 +1000

    clean script

--- app.R
+++ app.R
@@ -57,6 +57,7 @@ server <- shinyServer(function(input, output) {
     
     # economic threshold
     ET = input$controlcost/input$cerealvalue/(a*input$yieldpot)
+    # action threshold
     IT = ET/exp(b*input$controldelay)
     
     # function to calc losses from proportion TwRWA 
@@ -96,21 +97,9 @@ server <- shinyServer(function(input, output) {
       geom_rect(data=EIL, aes(xmin=xmin,ymin=ymin,xmax=xmax,ymax=ymax, fill=EIL)) +
       # RWA damage
       geom_abline(slope= a*input$yieldpot*input$cerealvalue/100, intercept=0, linetype=2) +
-      # geom_line(aes(100*Prop_tillers_w_RWA, RWAyieldloss), linetype=2) + 
       annotate("text", 1.5*RWAobspred$x[2]+2, 1.5*RWAobspred$y[2],hjust=0,
                     label = "RWA impact line", size=labelsize) +
-  
-      # geom_vline(xintercept = 100*IT, alpha=0.5, color = pal[2]) +
-      # annotate("text", x=100*IT-1, y=mean(d$RWAyieldloss[(nn/2):nn]), label = "action threshold", 
-      #           color = pal[2], angle = 90, size=labelsize) +
-      # geom_vline(xintercept = 100*ET, alpha=0.5, color = pal[4]) +
-      # geom_text(aes(100*ET-1, 10, label = "economic threshold"), color = pal[4], angle = 90) +
-      # control cost
-      # geom_hline(yintercept = input$controlcost, alpha=0.5, color=pal[5]) +
-      # annotate("text", x=30, y=input$controlcost+3, label = "control cost ($/ha)", 
-      #          size=labelsize, color = pal[5]) +
       geom_point(data=RWAobspred, aes(x, y, shape=key), size = 4) + 
-      # geom_line( data=RWAobspred, aes(x, y), linetype=2, size = 1.5) + 
       theme_bw() +
       scale_fill_manual(values = c("#c2ffa1", "#ff6047")) +
       scale_x_continuous(expand=c(0,0), breaks = seq(0,100,by=5)) +
@@ -121,7 +110,7 @@ server <- shinyServer(function(input, output) {
             axis.title = element_text(size=16),
             axis.text = element_text(size=12), plot.margin = margin(1, 2, 1, 1, "cm"),
             legend.position = "bottom",  legend.direction="vertical")+
-      scale_shape_manual(values = c(1, 2, 3, 4)) +
+      scale_shape_manual(values = c(1, 2, 3, 0)) +
       guides(shape = guide_legend(""), fill = guide_legend(""))
       
   })
